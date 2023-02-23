218a52985240eb3b5837ed141c57463abb27e37b
itpark <a.m.smyslov@yandex.ru>
2023-02-23 18:25:45 +0300

    done todolist

--- script.js
+++ script.js
@@ -1,47 +1,56 @@
 class TodoItem {
-    #id;
-    #date;
-    #description;
+    id;
+    date;
+    description;
 
     constructor(id, date, description) {
-        this.#id = id;
-        this.#date = date;
-        this.#description = description;
+        this.id = id;
+        this.date = date;
+        this.description = description;
     }
 
     getId() {
-        return this.#id;
+        return this.id;
     }
 
     getDate() {
-        return this.#date;
+        return this.date;
     }
 
     getDescription() {
-        return this.#description;
+        return this.description;
     }
 }
 
 class TodoItemsManager {
-    #globalId;
-    #todoItems;
+    globalId;
+    todoItems;
 
     constructor() {
-        this.#globalId = 0;
-        this.#todoItems = [];
+        this.globalId = 0;
+        this.todoItems = [];
     }
 
-    addNewTodoItem(date, description) {
-        this.#globalId++;
+    addNew(date, description) {
+        this.globalId++;
 
-        let todoItem = new TodoItem(this.#globalId, date, description);
+        let todoItem = new TodoItem(this.globalId, date, description);
 
-        this.#todoItems.push(todoItem);
+        this.todoItems.push(todoItem);
     }
 
-    getAllTodoItems() {
-        return this.#todoItems;
+    getAll() {
+        return this.todoItems;
     }
+
+    deleteById(id) {
+        let findIndex = this.todoItems.findIndex(todoItem => {
+            return todoItem.getId() === id;
+        });
+
+        this.todoItems.splice(findIndex, 1);
+    }
+
 }
 
 let todoItemsManager = new TodoItemsManager();
@@ -50,21 +59,98 @@ let inputDateField = document.getElementById("input-date-field");
 let inputDescriptionField = document.getElementById("input-description-field");
 let tableTodoItemsDiv = document.getElementById("table-todo-items-div");
 
+function padTo2Digits(num) {
+    return num.toString().padStart(2, '0');
+}
+
+function formatDateToYYYYMMDD(date) {
+    return [date.getFullYear(), padTo2Digits(date.getMonth() + 1), padTo2Digits(date.getDate())].join("-");
+}
+
+function formatDateToDDMMYYYY(date) {
+    return [padTo2Digits(date.getDate()), padTo2Digits(date.getMonth() + 1), date.getFullYear()].join(".");
+}
+
 function buttonAddTodoItem_Click() {
     let date = new Date(inputDateField.value);
     let description = inputDescriptionField.value;
 
-    //todo сделать проверку что введённая дата должна быть больше либо равна текущей дате
+    if (inputDateField.value.toString() === "") {
+        alert("Вы не ввели дату");
+        return;
+    }
+
+    date.setHours(0, 0, 0, 0);
+    let currentDate = new Date().setHours(0, 0, 0, 0);
+
+    if (date < currentDate) {
+        alert("Введите дату больше либо равную текущей дате");
+        return;
+    }
 
     if (description === "") {
         alert("Ошибка. Поле с текстом задачи должно быть заполнено");
         return;
     }
 
-    todoItemsManager.addNewTodoItem(date, description);
+    todoItemsManager.addNew(date, description);
 
-    inputDateField.value = new Date();
+    inputDateField.value = formatDateToYYYYMMDD(new Date());
     inputDescriptionField.value = "";
 
-    console.log(todoItemsManager.getAllTodoItems());
+    showTodoItems();
+
+    localStorage.setItem("todoItemsManager", JSON.stringify(todoItemsManager));
 }
+
+function buttonDeleteTodoItem_Click(id) {
+    todoItemsManager.deleteById(id);
+    showTodoItems();
+}
+
+function showTodoItems() {
+    let html = "";
+
+    html += `<table class="table">`;
+    html += `<thead>
+        <tr>
+            <th>№</th>
+            <th>Дата</th>
+            <th>Задача</th>
+            <th>Действие</th>
+        </tr>
+        </thead>`;
+    html += `<tbody>`;
+
+    todoItemsManager.getAll().forEach((todoItem, index) => {
+        html += `<tr>
+                <td>${index + 1}</td>
+                <td>${formatDateToDDMMYYYY(todoItem.getDate())}</td>
+                <td>${todoItem.getDescription()}</td>
+                <td><button class="btn btn-danger" onclick="buttonDeleteTodoItem_Click(${todoItem.getId()})">Удалить</button></td>
+            </tr>`;
+    });
+
+    html += `</tbody>`;
+    html += `</table>`;
+
+    tableTodoItemsDiv.innerHTML = html;
+}
+
+window.onload = function () {
+    let json = localStorage.getItem("todoItemsManager");
+    if (json != null) {
+        let obj = JSON.parse(json);
+
+        todoItemsManager.globalId = obj.globalId;
+
+        let todos = [];
+        obj.todoItems.forEach(todo => {
+            todos.push(new TodoItem(todo.id, new Date(todo.date), todo.description));
+        })
+
+        todoItemsManager.todoItems = todos;
+
+        showTodoItems();
+    }
+};
